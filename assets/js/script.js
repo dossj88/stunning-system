7ef37e69b0bc6535a9c7ef1549621756e9367f68
jasondoss <jason.j.doss@gmail.com>
2022-11-16 16:35:01 -0800

    added new font family, add root for color scheme

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// add questions
+
+const questions = [
+  {
+    question:
+    choices:
+    answer:
+  }
+  {
+    question:
+    choices:
+    answer:
+  }
+  {
+    question:
+    choices:
+    answer:
+  }
+  {
+    question:
+    choices:
+    answer:
+  }
+  {
+    question:
+    choices:
+    answer:
+  }
+]
+
